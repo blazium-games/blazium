0d4df2a69ef23c3a120ee81540b1e43708cd0bf1
FireBrandMint <guilhermesouza-2011@hotmail.com>
2025-10-19 01:26:33 -0300

    Translated Q12  to c++, with the worst name imaginable. Maybe change it later.

---
+++ core/math/fint.h
@@ -0,0 +1,237 @@
+#pragma once
+
+#include <cstdint>
+#include "core/error/error_macros.h"
+#include "core/math/math_funcs.h"
+
+//https://stackoverflow.com/questions/605124/fixed-point-math-in-c
+//Q12 deterministic number.
+//Search 'Q notation' for more info.
+struct [[nodiscard]] FInt {
+    static const int32_t SHIFT_AMOUNT = 12; //12 is 4096
+    static const int64_t ONEL = 1 << SHIFT_AMOUNT;
+
+    int64_t raw_value;
+
+    constexpr FInt() :
+			raw_value(0) {}
+	constexpr FInt(int64_t interger) :
+			raw_value(interger << SHIFT_AMOUNT) {}
+    constexpr FInt(int64_t interger, int decimalCount) :
+            raw_value(raw_value = (interger << SHIFT_AMOUNT) / decimalCount) {}
+    
+    constexpr FInt operator+(const FInt p_d) const;
+	constexpr FInt &operator+=(const FInt p_d);
+	constexpr FInt operator-(const FInt p_d) const;
+	constexpr FInt &operator-=(const FInt p_d);
+	constexpr FInt &operator*=(const FInt p_d);
+	constexpr FInt operator*(const FInt p_v1) const;
+	constexpr FInt &operator/=(const FInt p_d);
+	constexpr FInt operator/(const FInt p_d) const;
+
+	constexpr FInt operator+(int64_t p_rvalue) const;
+	constexpr FInt &operator+=(int64_t p_rvalue);
+	constexpr FInt operator+(int32_t p_rvalue) const;
+	constexpr FInt &operator+=(int32_t p_rvalue);
+
+	constexpr FInt operator-(int64_t p_rvalue) const;
+	constexpr FInt &operator-=(int64_t p_rvalue);
+	constexpr FInt operator-(int32_t p_rvalue) const;
+	constexpr FInt &operator-=(int32_t p_rvalue);
+
+	constexpr FInt operator*(int64_t p_rvalue) const;
+	constexpr FInt &operator*=(int64_t p_rvalue);
+	constexpr FInt operator*(int32_t p_rvalue) const;
+	constexpr FInt &operator*=(int32_t p_rvalue);
+
+	constexpr FInt operator/(int64_t p_rvalue) const;
+	constexpr FInt &operator/=(int64_t p_rvalue);
+	constexpr FInt operator/(int32_t p_rvalue) const;
+	constexpr FInt &operator/=(int32_t p_rvalue);
+
+	constexpr FInt operator%(const FInt &p_v1) const;
+	constexpr FInt operator%(int64_t p_rvalue) const;
+	constexpr FInt &operator%=(int64_t p_rvalue);
+
+	constexpr FInt operator-() const;
+
+	constexpr bool operator==(const FInt &p_d) const;
+	constexpr bool operator!=(const FInt &p_d) const;
+	constexpr bool operator<(const FInt &p_d) const;
+	constexpr bool operator<=(const FInt &p_d) const;
+	constexpr bool operator>(const FInt &p_d) const;
+	constexpr bool operator>=(const FInt &p_d) const;
+};
+
+constexpr FInt &FInt::operator+=(const FInt p_d) {
+    
+	raw_value += p_d.raw_value;
+	return *this;
+}
+
+constexpr FInt FInt::operator+(const FInt p_d) const {
+	return { raw_value + p_d.raw_value };
+}
+
+constexpr FInt &FInt::operator-=(const FInt p_d) {
+    raw_value -= p_d.raw_value;
+	return *this;
+}
+
+constexpr FInt FInt::operator-(const FInt p_d) const {
+	return { raw_value - p_d.raw_value };
+}
+
+constexpr FInt &FInt::operator*=(const FInt p_d) {
+	raw_value = (raw_value * p_d.raw_value) >> SHIFT_AMOUNT;
+	return *this;
+}
+
+constexpr FInt FInt::operator*(const FInt p_d) const {
+	return { (raw_value * p_d.raw_value >> SHIFT_AMOUNT) };
+}
+
+constexpr FInt &FInt::operator/=(const FInt p_d) {
+	raw_value = (raw_value << SHIFT_AMOUNT) / p_d.raw_value;
+	return *this;
+}
+
+constexpr FInt FInt::operator/(const FInt p_d) const {
+	return FInt { (raw_value << SHIFT_AMOUNT) / p_d.raw_value };
+}
+
+//Foreign addition.
+
+constexpr FInt &FInt::operator+=(int64_t p_ot) {
+	raw_value += p_ot << FInt::SHIFT_AMOUNT;
+	return *this;
+}
+
+constexpr FInt &FInt::operator+=(int32_t p_ot) {
+	raw_value += ((int64_t)p_ot) << FInt::SHIFT_AMOUNT;
+	return *this;
+}
+
+constexpr FInt FInt::operator+(int64_t p_ot) const {
+	return FInt { raw_value + (p_ot << FInt::SHIFT_AMOUNT) };
+}
+
+constexpr FInt FInt::operator+(int32_t p_ot) const {
+	return FInt { raw_value + (p_ot << FInt::SHIFT_AMOUNT) };
+}
+
+constexpr FInt operator+(int32_t p_ot, const FInt &p_det) {
+	return p_det + (((int64_t)p_ot) << FInt::SHIFT_AMOUNT);
+}
+
+constexpr FInt operator+(int64_t p_ot, const FInt &p_det) {
+	return p_det + (p_ot << FInt::SHIFT_AMOUNT);
+}
+
+//Foreign substraction.
+
+constexpr FInt &FInt::operator-=(int64_t p_ot) {
+	raw_value -= p_ot << FInt::SHIFT_AMOUNT;
+	return *this;
+}
+
+constexpr FInt &FInt::operator-=(int32_t p_ot) {
+	raw_value -= ((int64_t)p_ot) << FInt::SHIFT_AMOUNT;
+	return *this;
+}
+
+constexpr FInt FInt::operator-(int64_t p_ot) const {
+	return FInt { raw_value - (p_ot << FInt::SHIFT_AMOUNT) };
+}
+
+constexpr FInt FInt::operator-(int32_t p_ot) const {
+	return FInt { raw_value - (p_ot << FInt::SHIFT_AMOUNT) };
+}
+
+constexpr FInt operator-(int32_t p_ot, const FInt &p_det) {
+	return p_det - (((int64_t)p_ot) << FInt::SHIFT_AMOUNT);
+}
+
+constexpr FInt operator-(int64_t p_ot, const FInt &p_det) {
+	return p_det - (p_ot << FInt::SHIFT_AMOUNT);
+}
+
+//Foreign multiplication.
+
+constexpr FInt &FInt::operator*=(int64_t p_ot) {
+	raw_value *= p_ot;
+	return *this;
+}
+
+constexpr FInt &FInt::operator*=(int32_t p_ot) {
+	raw_value *= p_ot;
+	return *this;
+}
+
+constexpr FInt FInt::operator*(int64_t p_ot) const {
+	return FInt { raw_value * p_ot };
+}
+
+constexpr FInt FInt::operator*(int32_t p_ot) const {
+	return FInt { raw_value * p_ot };
+}
+
+constexpr FInt operator*(int32_t p_ot, const FInt &p_det) {
+	return p_det * p_ot;
+}
+
+constexpr FInt operator*(int64_t p_ot, const FInt &p_det) {
+	return p_det * p_ot;
+}
+
+//Foreign division
+
+constexpr FInt &FInt::operator/=(int64_t p_ot) {
+	raw_value *= p_ot;
+	return *this;
+}
+
+constexpr FInt FInt::operator/(int64_t p_ot) const {
+	return FInt { raw_value / p_ot };
+}
+
+constexpr FInt &FInt::operator/=(int32_t p_ot) {
+	raw_value *= p_ot;
+	return *this;
+}
+
+constexpr FInt FInt::operator/(int32_t p_ot) const {
+	return FInt { raw_value / p_ot };
+}
+
+constexpr FInt FInt::operator-() const {
+	return { -raw_value };
+}
+
+constexpr bool FInt::operator==(const FInt &p_d) const {
+	return raw_value == p_d.raw_value;
+}
+
+constexpr bool FInt::operator!=(const FInt &p_d) const {
+	return raw_value != p_d.raw_value;
+}
+
+constexpr bool FInt::operator<(const FInt &p_d) const {
+	return raw_value < p_d.raw_value;
+}
+
+constexpr bool FInt::operator<=(const FInt &p_d) const {
+	return raw_value <= p_d.raw_value;
+}
+
+constexpr bool FInt::operator>(const FInt &p_d) const {
+	return raw_value > p_d.raw_value;
+}
+
+constexpr bool FInt::operator>=(const FInt &p_d) const {
+	return raw_value >= p_d.raw_value;
+}
+
+constexpr bool FInt::operator==(const FInt &p_d) const {
+	return raw_value == p_d.raw_value;
+}
