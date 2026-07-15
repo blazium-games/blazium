KleirRampage45/redot-engine

[Windows] Add Intel Gen9.5 (Kaby Lake) GPUs to Angle blocklist.

main/main.cpp
@@ -2369,6 +2369,34 @@ Error Main::setup(const char *execpath, int argc, char *argv[], bool p_second_ph
 		FORCE_ANGLE("0x8086", "0x193B"); // Intel(R) Iris Pro Graphics 580, Gen9, Skylake
 		FORCE_ANGLE("Intel", "Intel(R) Iris Pro Graphics P580");
 		FORCE_ANGLE("0x8086", "0x193D"); // Intel(R) Iris Pro Graphics P580, Gen9, Skylake
+		FORCE_ANGLE("Intel", "Intel(R) HD Graphics 610");
+		FORCE_ANGLE("0x8086", "0x5902"); // Intel(R) HD Graphics 610, Gen9.5, Kaby Lake
+		FORCE_ANGLE("0x8086", "0x5906");
+		FORCE_ANGLE("0x8086", "0x5908");
+		FORCE_ANGLE("0x8086", "0x590A");
+		FORCE_ANGLE("0x8086", "0x590B");
+		FORCE_ANGLE("0x8086", "0x590E");
+		FORCE_ANGLE("Intel", "Intel(R) HD Graphics 615");
+		FORCE_ANGLE("0x8086", "0x5913"); // Intel(R) HD Graphics 615, Gen9.5, Kaby Lake
+		FORCE_ANGLE("0x8086", "0x5915");
+		FORCE_ANGLE("0x8086", "0x591E");
+		FORCE_ANGLE("Intel", "Intel(R) HD Graphics 620");
+		FORCE_ANGLE("0x8086", "0x5916"); // Intel(R) HD Graphics 620, Gen9.5, Kaby Lake
+		FORCE_ANGLE("0x8086", "0x5917");
+		FORCE_ANGLE("0x8086", "0x5921");
+		FORCE_ANGLE("Intel", "Intel(R) HD Graphics 630");
+		FORCE_ANGLE("0x8086", "0x5912"); // Intel(R) HD Graphics 630, Gen9.5, Kaby Lake
+		FORCE_ANGLE("0x8086", "0x591B");
+		FORCE_ANGLE("Intel", "Intel(R) HD Graphics 635");
+		FORCE_ANGLE("0x8086", "0x5923"); // Intel(R) HD Graphics 635, Gen9.5, Kaby Lake
+		FORCE_ANGLE("Intel", "Intel(R) Iris Plus Graphics 640");
+		FORCE_ANGLE("0x8086", "0x5926"); // Intel(R) Iris Plus Graphics 640, Gen9.5, Kaby Lake
+		FORCE_ANGLE("Intel", "Intel(R) Iris Plus Graphics 650");
+		FORCE_ANGLE("0x8086", "0x5927"); // Iris Plus Graphics 650, Gen9.5, Kaby Lake
+		FORCE_ANGLE("0x8086", "0x593B");
+		FORCE_ANGLE("Intel", "Intel(R) HD Graphics P630");
+		FORCE_ANGLE("0x8086", "0x591A"); // Intel(R) HD Graphics P630, Gen9.5, Kaby Lake
+		FORCE_ANGLE("0x8086", "0x591D");
 
 #undef FORCE_ANGLE
 

blazium-games/blazium

main/main.cpp
@@ -2420,6 +2420,34 @@ Error Main::setup(const char *execpath, int argc, char *argv[], bool p_second_ph
 		BLOCK_DEVICE("0x8086", "0x193B"); // Intel(R) Iris Pro Graphics 580, Gen9, Skylake
 		BLOCK_DEVICE("Intel", "Intel(R) Iris Pro Graphics P580");
 		BLOCK_DEVICE("0x8086", "0x193D"); // Intel(R) Iris Pro Graphics P580, Gen9, Skylake
+		BLOCK_DEVICE("Intel", "Intel(R) HD Graphics 610");
+		BLOCK_DEVICE("0x8086", "0x5902"); // Intel(R) HD Graphics 610, Gen9.5, Kaby Lake
+		BLOCK_DEVICE("0x8086", "0x5906");
+		BLOCK_DEVICE("0x8086", "0x5908");
+		BLOCK_DEVICE("0x8086", "0x590A");
+		BLOCK_DEVICE("0x8086", "0x590B");
+		BLOCK_DEVICE("0x8086", "0x590E");
+		BLOCK_DEVICE("Intel", "Intel(R) HD Graphics 615");
+		BLOCK_DEVICE("0x8086", "0x5913"); // Intel(R) HD Graphics 615, Gen9.5, Kaby Lake
+		BLOCK_DEVICE("0x8086", "0x5915");
+		BLOCK_DEVICE("0x8086", "0x591E");
+		BLOCK_DEVICE("Intel", "Intel(R) HD Graphics 620");
+		BLOCK_DEVICE("0x8086", "0x5916"); // Intel(R) HD Graphics 620, Gen9.5, Kaby Lake
+		BLOCK_DEVICE("0x8086", "0x5917");
+		BLOCK_DEVICE("0x8086", "0x5921");
+		BLOCK_DEVICE("Intel", "Intel(R) HD Graphics 630");
+		BLOCK_DEVICE("0x8086", "0x5912"); // Intel(R) HD Graphics 630, Gen9.5, Kaby Lake
+		BLOCK_DEVICE("0x8086", "0x591B");
+		BLOCK_DEVICE("Intel", "Intel(R) HD Graphics 635");
+		BLOCK_DEVICE("0x8086", "0x5923"); // Intel(R) HD Graphics 635, Gen9.5, Kaby Lake
+		BLOCK_DEVICE("Intel", "Intel(R) Iris Plus Graphics 640");
+		BLOCK_DEVICE("0x8086", "0x5926"); // Intel(R) Iris Plus Graphics 640, Gen9.5, Kaby Lake
+		BLOCK_DEVICE("Intel", "Intel(R) Iris Plus Graphics 650");
+		BLOCK_DEVICE("0x8086", "0x5927"); // Iris Plus Graphics 650, Gen9.5, Kaby Lake
+		BLOCK_DEVICE("0x8086", "0x593B");
+		BLOCK_DEVICE("Intel", "Intel(R) HD Graphics P630");
+		BLOCK_DEVICE("0x8086", "0x591A"); // Intel(R) HD Graphics P630, Gen9.5, Kaby Lake
+		BLOCK_DEVICE("0x8086", "0x591D");
 
 #undef BLOCK_DEVICE
 